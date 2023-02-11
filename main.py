53b5b56bd6907beb12c935ff213f1584bdc44466
Matěj Richter <richterm22@spstrutnov.cz>
2023-02-11 17:20:57 +0100

    Display + shutdown of app

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ import random
 # framework start
 pygame.init()
 
+#Basic parameters
+Resolution_X = 1280
+Resolution_Y = 720
+
+
+#Window creation
+window = pygame.display.set_mode((Resolution_X, Resolution_Y))
+pygame.display.set_caption('Vending machine game')
+
+#window loop
+while True:
+    #Turning off the app
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           #framework shutdown
+            pygame.quit()
+            #app shutdown
+            sys.exit()
+
