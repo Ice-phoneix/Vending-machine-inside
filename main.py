b1649c4da457eac6df40f4c7980e6fb0f51e902f
Matěj Richter <richterm22@spstrutnov.cz>
2023-02-15 14:25:47 +0100

    Start of drinks select indicator

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import random
 pygame.init()
 
 #Basic parameters
-Resolution_X = 1280
-Resolution_Y = 720
-
+Resolution_X = 1920
+Resolution_Y = 1090
+White = (255, 255, 255)
+Background = pygame.image.load('Images/Game background.jpg')
 
 #Window creation
 window = pygame.display.set_mode((Resolution_X, Resolution_Y))
@@ -22,6 +23,13 @@ while True:
             pygame.quit()
             #app shutdown
             sys.exit()
+
+    window.fill(White)
+    
+    window.blit(Background, (0, 0)) 
+            
+            
+            
     #display update
     pygame.display.update()
 
